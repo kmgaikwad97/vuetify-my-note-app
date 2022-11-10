bca929692359edde11217adff11505ace544711b
kshitij2022 <kshitijwohlig@gmail.com>
2022-11-10 12:58:35 +0530

    menus added delete task done

--- src/components/Todo/Task.vue
+++ src/components/Todo/Task.vue
@@ -17,36 +17,30 @@
         </v-list-item-content>
         <!-- <v-list-item-action @click.stop="$store.dispatch('deleteTask', task.id)"> -->
         <v-list-item-action>
-          <v-btn @click.stop="dialogs.delete = true" icon>
+          <!-- <v-btn @click.stop="dialogs.delete = true" icon>
             <v-icon color="primary lighten-1">mdi-delete</v-icon>
-          </v-btn>
+          </v-btn> -->
+          <task-menu :task="task"/>
         </v-list-item-action>
       </template>
     </v-list-item>
 
     <v-divider></v-divider>
 
-    <dialog-delete
+    <!-- <dialog-delete
       v-if="dialogs.delete"
       @close="dialogs.delete = false"
       :task="task"
-    />
+    /> -->
   </div>
 </template>
 
 <script>
 export default {
   props: ["task"],
-  data() {
-    return {
-      dialogs: {
-        delete: false,
-      },
-    };
-  },
   components: {
-    "dialog-delete": require("@/components/Todo/Dialogs/DialogDelete.vue")
-      .default,
+    "task-menu": require("@/components/Todo/TaskMenu.vue")
+      .default
   },
 };
 </script>
